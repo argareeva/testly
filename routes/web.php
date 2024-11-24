55c1cd69329b2615c6aae152e17fa78ffcb618a7
argareeva <gareeva0223@gmail.com>
2024-11-24 23:37:15 +0100

    Added missed standard files

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FeedbackController;
 
@@ -11,6 +12,7 @@ Route::get('/applications/{application}/feedback', [App\Http\Controllers\Feedbac
 Route::post('/applications/{application}/feedback', [App\Http\Controllers\FeedbackController::class, 'store'])->name('feedback.store');
 
 //Authenticated routes
+require __DIR__.'/auth.php';
 Route::name('user.')->group(function() {
     Route::resource('user/applications', App\Http\Controllers\User\ApplicationController::class);
 });
@@ -19,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
