356a0113f68f20a1237e17f3cc74bf61bd646a3d
argareeva <gareeva0223@gmail.com>
2024-11-26 18:29:52 +0100

    Switched to CDN for assets (instead of vite)

--- resources/views/layouts/site/layout.blade.php
+++ resources/views/layouts/site/layout.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles / Scripts -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
