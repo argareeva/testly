0fe19d9071f37568138e7cf5be04b271e9003c89
argareeva <gareeva0223@gmail.com>
2025-01-25 18:18:51 +0100

    Removed unused dashboard route

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,6 @@ Route::prefix('user')->name('user.')->middleware(['auth', 'verified'])->group(fu
     Route::get('tested', [\App\Http\Controllers\User\TestedController::class, 'index'])->name('tested.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
