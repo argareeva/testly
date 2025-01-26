b41d822b4ff5c99bf3d55fd0277628c0ca28d595
argareeva <gareeva0223@gmail.com>
2025-01-27 00:28:21 +0100

    Fixed bug with route naming

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ Route::name('api.')->group( function() {
     Route::get('applications/{id}', [\App\Http\Controllers\Api\ApplicationController::class, 'show'] )->name('applications.show');
     Route::get('authors', [\App\Http\Controllers\Api\AuthorController::class, 'index'] )->name('authors.index');
     Route::get('authors/{id}', [\App\Http\Controllers\Api\AuthorController::class, 'show'] )->name('authors.show');
-    Route::get('categories', [\App\Http\Controllers\Api\CategoryController::class, 'index'] )->name('authors.index');
-    Route::get('categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'show'] )->name('authors.show');
+    Route::get('categories', [\App\Http\Controllers\Api\CategoryController::class, 'index'] )->name('categories.index');
+    Route::get('categories/{id}', [\App\Http\Controllers\Api\CategoryController::class, 'show'] )->name('categories.show');
 });
